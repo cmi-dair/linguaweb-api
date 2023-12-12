d434bee162d166375c3a4dcb672ec99b0f98d31c
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-12-12 15:03:08 -0500

    Add case insensitivity test to check word

--- tests/endpoint/conftest.py
+++ tests/endpoint/conftest.py
@@ -1,7 +1,5 @@
 """Fixtures and configurations for testing the endpoints of the CTK API."""
-import contextlib
 import enum
-import os
 
 import pytest
 import sqlalchemy
@@ -15,15 +13,6 @@ API_ROOT = "/api/v1"
 
 
 @pytest.fixture(scope="session", autouse=True)
-def _set_env() -> None:
-    """Sets the environment variables.
-
-    For mocking with moto, the s3 endpoint must be None.
-    """
-    with contextlib.suppress(KeyError):
-        del os.environ["LWAPI_S3_ENDPOINT_URL"]
-
-
 class Endpoints(str, enum.Enum):
     """Enum class that represents the available endpoints for the API."""
 
--- tests/endpoint/test_words.py
+++ tests/endpoint/test_words.py
@@ -92,6 +92,20 @@ def test_post_check_word(
     assert response.json() is True
 
 
+def test_post_check_word_case_insensitive(
+    word: models.Word,
+    client: testclient.TestClient,
+    endpoints: conftest.Endpoints,
+) -> None:
+    """Tests the check word endpoint."""
+    endpoint = endpoints.POST_CHECK_WORD.format(word_id=word.id)
+
+    response = client.post(endpoint, data={"word": word.word.upper()})
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() is True
+
+
 @moto.mock_s3
 def test_get_audio(
     mocker: pytest_mock.MockFixture,
